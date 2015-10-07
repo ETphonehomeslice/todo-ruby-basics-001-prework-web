8d65c6e338b2e54dbdd1d883ae08b67d4ce57bfa
ETphonehomeslice <et88guy@hotmail.com>
2015-10-07 13:30:57 -0400

    Done

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(type)
+  type
 end
 
-def greeting
+def greeting(type1, type2)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(type = "cheese")
+  type
 end
